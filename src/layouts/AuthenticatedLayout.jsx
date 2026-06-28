70e454212fec96eab58ed573bd980cf128c3a4b2
zeimo_03 <zydrxpinedazuno@gmail.com>
2026-06-28 22:20:28 +0800

    nilagyan ko lang transition kapag nagna-navigate ng pages

--- src/layouts/AuthenticatedLayout.jsx
+++ src/layouts/AuthenticatedLayout.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import Sidebar from '../components/Sidebar/Sidebar';
+import './AuthenticatedLayout.css';
 
 export default function AuthenticatedLayout() {
+  const location = useLocation();
+
   return (
     <div className="dashboard-container">
       <Sidebar />
       <main className="authenticated-main">
-        <Outlet />
+        {/* key={pathname} forces this wrapper to remount on every page
+            change, which re-triggers the fade/slide-up entrance animation */}
+        <div className="page-transition" key={location.pathname}>
+          <Outlet />
+        </div>
       </main>
     </div>
   );
